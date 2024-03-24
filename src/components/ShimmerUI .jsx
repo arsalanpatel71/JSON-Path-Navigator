80653a47cb5bbc4429ef57d15a8e447569654a24
arsalanpatel71 <162526772+arsalanpatel71@users.noreply.github.com>
2024-03-24 07:45:13 +0530

    Add files via upload

---
+++ src/components/ShimmerUI .jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import "../App.css";
+
+const ShimmerUI = () => {
+  const lineStyles = {
+    marginBottom: "20px",
+    height: "8px",
+  };
+
+  return (
+    <div className="shimmer-container">
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "55%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "80%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "80%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "55%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "80%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "80%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "55%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "80%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "80%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "80%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "55%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "80%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "55%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "55%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "80%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "80%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "45%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "80%" }}
+      ></div>
+      <div
+        className="shimmer-line"
+        style={{ ...lineStyles, width: "80%" }}
+      ></div>
+    </div>
+  );
+};
+
+export default ShimmerUI;
